464ce4d6e53901e6b15cdd2269cb1a31c99ff0e8
Hao C <impyworm@gmail.com>
2023-05-28 01:56:59 -0600

    Set up Next.js
    Co-authored-by: sireButItsUnique <robertcxlee@gmail.com>
    Co-authored-by: wdotmathree <wdotmathree@gmail.com>

---
+++ web/src/app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+	title: "Corrosion Language Documentation",
+	description: "Documentation and usage for the Corrosion language",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
