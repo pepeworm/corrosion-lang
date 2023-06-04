d66043a802c99573c5c0ec60c12f5040ca260cb6
Hao C <impyworm@gmail.com>
2023-06-04 14:05:43 -0600

    Install MDX

--- web/src/app/docs/[docId]/page.js
+++ web/src/app/docs/[docId]/page.js
@@ -2,26 +2,19 @@
 
 import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
-import ReactMarkdown from "react-markdown";
 
 export default function Home() {
 	const url = usePathname();
 	const currPage = url.match(/([^\/]*)$/)[1];
+	const mdPath = `@/data/docs/${currPage}.md`;
 
 	const [content, setContent] = useState();
 
-	useEffect(() => {
-		const mdPath = `@/data/docs/${currPage}.md`;
 
-		fetch(mdPath)
-			.then((res) => res.text())
-			.then((text) => setContent(text));
-	}, []);
 
 	return (
 		<section className="pt-14 pl-[20.5rem]">
 			<div className="text-left ml-16">
-				<ReactMarkdown source />
 				{/* <h1 className="gradient header">{content}</h1> */}
 				{/* <h1 className="header">A C-like Language with Fragments of Rust</h1> */}
 			</div>
